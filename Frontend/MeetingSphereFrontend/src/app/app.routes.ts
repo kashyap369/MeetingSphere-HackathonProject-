f39164138c69c78fc463e329dedf300d18479658
Shubham <shubham983346@gmail.com>
2025-10-11 12:04:31 +0530

    Landing Page skeleton created

--- Frontend/MeetingSphereFrontend/src/app/app.routes.ts
+++ Frontend/MeetingSphereFrontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LangingPage } from './LandingPage/langing-page/langing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',component:LangingPage
+    }
+];
