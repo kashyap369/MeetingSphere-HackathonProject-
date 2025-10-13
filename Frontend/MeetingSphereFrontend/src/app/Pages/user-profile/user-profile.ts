03ea4dda7ead55cc897fd46b8fc86bab74035ac6
Shubham <shubham983346@gmail.com>
2025-10-13 23:19:29 +0530

    Static Website with dummy Data and page setup stage 1 Compeleted

--- Frontend/MeetingSphereFrontend/src/app/Pages/user-profile/user-profile.ts
+++ Frontend/MeetingSphereFrontend/src/app/Pages/user-profile/user-profile.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Calendar, Clock, Download, Hourglass, LUCIDE_ICONS, LucideAngularModule, LucideIconProvider, Share2, Users } from 'lucide-angular';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  standalone: true,
+  imports: [LucideAngularModule],
   templateUrl: './user-profile.html',
-  styleUrl: './user-profile.css'
+  styleUrl: './user-profile.css',
+  providers:[
+    {
+      provide:LUCIDE_ICONS,
+      multi:true,
+      useValue:new LucideIconProvider({Calendar,Clock,Users,Hourglass,Download,Share2})
+    }
+  ]
 })
 export class UserProfile {
 
